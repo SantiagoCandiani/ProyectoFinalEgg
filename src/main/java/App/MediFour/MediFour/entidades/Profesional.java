8ea4ad064a5099f3faa5b781df15401ed5b28c39
Candiani, Santiago <s.candiani@hotmail.com>
2023-09-03 17:19:55 -0300

    Agregue varias cosas, nada terminado....

--- src/main/java/App/MediFour/MediFour/entidades/Profesional.java
+++ src/main/java/App/MediFour/MediFour/entidades/Profesional.java
@@ -2,6 +2,7 @@ package App.MediFour.MediFour.entidades;
 
 import App.MediFour.MediFour.enumeraciones.DiaSemana;
 import App.MediFour.MediFour.enumeraciones.Especialidad;
+import java.io.Serializable;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +12,15 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Getter
@@ -24,7 +28,12 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "Profesional")
-public class Profesional extends Usuario {
+public class Profesional extends Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "uuid") //genera un valor de manera automatica al momento que el repo persita la entidad.
+    @GenericGenerator(name = "uuid", strategy = "uuid2") //es una estrategia alternativa
+    private String id;
 
     protected String matricula;
     @Enumerated(EnumType.STRING)
@@ -33,11 +42,15 @@ public class Profesional extends Usuario {
     @CollectionTable(name = "Profesional_DiasDisponibles")
     @Enumerated(EnumType.STRING)
     protected List<DiaSemana> diasDisponibles;
-    protected LocalTime  horarioEntrada;
-    protected LocalTime  horarioSalida;
+    protected LocalTime horarioEntrada;
+    protected LocalTime horarioSalida;
     protected Double precioConsulta;
     protected Double reputacion;
     protected String observaciones;
+
     @OneToMany(mappedBy = "profesional", fetch = FetchType.LAZY)
     private List<Turno> turnosCreados = new ArrayList<>();
+
+    @OneToMany
+    private List<FichaMedica> historialConsultas;
 }
--- src/main/java/App/MediFour/MediFour/repositorios/TurnoRepositorio.java
+++ src/main/java/App/MediFour/MediFour/repositorios/TurnoRepositorio.java
@@ -1,19 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package App.MediFour.MediFour.repositorios;
 
+import App.MediFour.MediFour.entidades.Paciente;
+import App.MediFour.MediFour.entidades.Profesional;
 import App.MediFour.MediFour.entidades.Turno;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Usuario
- */
 @Repository
 public interface TurnoRepositorio extends JpaRepository<Turno, String> {
+
+    @Query("SELECT t FROM Turno t WHERE t.profesional = :profesional")
+    public List<Turno> buscarTurnosProfesional(@Param("profesional") Profesional profesional);
+
+    @Query("SELECT t FROM Turno t WHERE t.paciente = :paciente")
+    public List<Turno> buscarTurnosPaciente(@Param("paciente") Paciente paciente);
+
+    @Query("SELECT DISTINCT t.paciente FROM Turno t WHERE t.profesional.id = :profesionalId")
+    List<Paciente> obtenerNombresPacientesConTurnoPorMedico(@Param("profesionalId") String profesionalId);
 
 }
