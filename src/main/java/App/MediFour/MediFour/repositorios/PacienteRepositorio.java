5973c5c29f8046827e54d20611ca05319d73cf47
Candiani, Santiago <s.candiani@hotmail.com>
2023-09-05 02:08:42 -0300

    Modificar paciente casi funcionando. Modifica en la DB pero no renderiza

--- src/main/java/App/MediFour/MediFour/repositorios/PacienteRepositorio.java
+++ src/main/java/App/MediFour/MediFour/repositorios/PacienteRepositorio.java
@@ -13,13 +13,18 @@ public interface PacienteRepositorio extends JpaRepository<Paciente, String> {
 
     @Query("SELECT p FROM Paciente p WHERE p.dni = :dni")
     public Paciente buscarPorDNI(@Param("dni") Integer dni);
-   
-    
+
     @Query("SELECT p FROM Paciente p WHERE p.obraSocial = :obraSocial")
     public List<Paciente> buscarPorObraSocial(@Param("obraSocial") ObraSocial obraSocial);
 
     @Query("SELECT p FROM Paciente p WHERE p.id = :id")
     public Paciente buscarPorId(@Param("id") String id);
+
+    @Query("SELECT p FROM Paciente p WHERE p.id = :id")
+    public Paciente buscarPacientePorID(@Param("id") String id);
+
+    @Query("SELECT m FROM Paciente m WHERE m.email = :email")
+    public Paciente buscarPorEmail(@Param("email") String email);
 
     List<Paciente> findByActivoTrue(); // Utiliza el nombre de método con la convención para filtrar pacientes activos
 
